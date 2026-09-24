8e754e9b67f0c720fd65d1bc78c128437adcf3ba
siahaanangel2008-droid <siahaanangel2008@gmail.com>
2026-09-24 16:03:29 +0700

    Tambahkan seluruh tugas

---
+++ input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    float IPK;
+    char kalimat[100];
+    
+
+    printf("masukkan IPK : ");
+    scanf("%f" , &IPK);
+
+    printf("IPK saya = %.1f", IPK);
+
+
+
+
+}
---
+++ output.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+    int umur = 18;
+
+    printf("%d", umur);
+
+    char kalimat[100] = "saya adalah mahasiswa TI"}
